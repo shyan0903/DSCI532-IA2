9355fdba64009be66856e3066f63c0331bfa6cc9
irene93 <irene93@student.ubc.ca>
2022-03-10 11:30:40 -0800

    Finish implementation

--- app.R
+++ app.R
@@ -9,17 +9,10 @@ library(dplyr)
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
 # Read in the raw data and subset the data for analysis
-df <- readr::read_csv(here::here('data', 'world-data-gapminder_raw.csv')) %>% 
-    select(country, year, population, region, income)
+df <- readr::read_csv(here::here('data', 'combined-data-for-GDP-per-cap.csv'))
 
 # Define constant values
-years <- list("1901" = "1901",
-              "1908" = "1908",
-              "1918" = "1918", 
-              "1928" = "1928",
-              "1938" = "1938",
-              "1948" = "1948",
-              "1958" = "1958",
+years <- list("1960" = "1960",
               "1968" = "1968",
               "1978" = "1978",
               "1988" = "1988",
@@ -27,13 +20,12 @@ years <- list("1901" = "1901",
               "2008" = "2008",
               "2018" = "2018"
               )
-INCOME_UNIT <- 10000000
-regions <- list("Asia" = "Asia",
-                "Europe" = "Europe",
-                "Africa" = "Africa",
-                "Americas" = "Americas",
-                "Oceania" = "Oceania"
-                )
+regions <- list(list(label = "All", value = ''),
+                list(label = "Asia", value = "Asia"),
+                list(label = "Europe", value = "Europe"),
+                list(label = "Africa", value = "Africa"),
+                list(label = "Americas", value = "Americas"),
+                list(label = "Oceania", value = "Oceania"))
     
 COUNTRIES <- unique(df$country)
 
@@ -44,65 +36,62 @@ app$layout(
             htmlLabel("Zoom in Years: "),
             dccRangeSlider(
                 id = 'year_slider',
-                min = 1901, 
+                min = 1960, 
                 max = 2018,
-                value = list(1901, 2018),
-                marks = years
-            )
-            # ,
-            # htmlLabel("Filter by Geographic Region: "),
-            # dccDropdown(
-            #     id = 'region_dropdown',
-            #     options = regions , 
-            #     value = '')
+                step = 1,
+                value = list(1960, 2018),
+                marks = years,
+                tooltip = 'placement'
+            ),
+            htmlLabel("Filter by Geographic Region: "),
+            dccDropdown(
+                id = 'region_dropdown',
+                options = regions,
+                value = '',
+                multi = TRUE)
         )
     )
 )
 
 app$callback(
     output('plot-area', 'figure'),
-    list(input('year_slider', 'value')
-         # ,
-         # input('region_dropdown', 'value')
-         ),
+    list(input('year_slider', 'value'),
+         input('region_dropdown', 'value')),
     
-    function(year_range) {
-    
-        p <- df %>%
-            filter(year >= year_range[1] & year <= year_range[2]) %>%
-            group_by(year) %>%
-            summarise(income = sum(income),
-                      pop = sum(population)) %>%
-            mutate(income_per_capita = income * INCOME_UNIT / pop) %>%
-            ggplot(aes(x = year, y = income_per_capita)) +
-            geom_line() +
-            labs(x = "Year",
-                 y = "Income Per Capita (in US$)",
-                 title = "Income Per Capita Has Been Rising") +
-            ggthemes::scale_color_tableau()
-
-        ggplotly(p)
+    function(year_range, region_filter) {
+        df_sub <- df %>%
+            filter(year >= year_range[1] & year <= year_range[2])
         
+        if ('' %in% region_filter) {
+            p <- df_sub %>%
+                group_by(year) %>%
+                summarise(income = sum(`GDP total`),
+                          pop = sum(population)) %>%
+                mutate(income_per_capita = income / pop) %>%
+                ggplot(aes(x = year, y = income_per_capita)) +
+                geom_line() +
+                labs(x = "Year",
+                     y = "income Per Capita (in US$)",
+                     title = "income Per Capita Has Been Rising") +
+                ggthemes::scale_color_tableau()
+        } else {
+            df_sub <- df_sub %>%
+                filter(region %in% region_filter)
+            p <- df_sub %>%
+                group_by(year, region) %>%
+                summarise(income = sum(`GDP total`),
+                          pop = sum(population)) %>%
+                mutate(income_per_capita = income / pop) %>%
+                ggplot(aes(x = year, y = income_per_capita, color=region)) +
+                geom_line() +
+                labs(x = "Year",
+                     y = "income Per Capita (in US$)",
+                     title = "Income Per Capita Has Been Rising") +
+                ggthemes::scale_color_tableau()
+        }
+        ggplotly(p)
 
     }
-    # function(time) {
-    #     df_filter_year <- df %>% 
-    #         filter(year >= years[time[1] + 1] & year <= years[time[2] + 1])
-    #     
-    #     if (region == '') {
-
-    #     } else {
-    #         p <- df_filter_year %>%
-    #             filter(region == region) %>%
-    #             group_by(year) %>%
-    #             summarise(income = sum(income),
-    #                       pop = sum(population)) %>%
-    #             mutate(income_per_capita = income * INCOME_UNIT / pop) %>%
-    #             ggplot(aes(x = year, y = income_per_capita)) +
-    #             geom_line()
-    #         ggplotly(p)
-    #     }
-    # }
 )
 
 app$run_server(host = '0.0.0.0')
