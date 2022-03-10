cc3029b79dfb165e17d99c0df9a92d454fb10e48
irene93 <irene93@student.ubc.ca>
2022-03-10 09:55:55 -0800

    Fix year slider

--- app.R
+++ app.R
@@ -13,38 +13,96 @@ df <- readr::read_csv(here::here('data', 'world-data-gapminder_raw.csv')) %>%
     select(country, year, population, region, income)
 
 # Define constant values
-YEAR_MIN <- 1901
-YEAR_MAX <- 2018
-YEAR_INTERVAL <- 10
-INCOME_UNIT <- 1000000
-REGIONS <- unique(df$region)
+years <- list("1901" = "1901",
+              "1908" = "1908",
+              "1918" = "1918", 
+              "1928" = "1928",
+              "1938" = "1938",
+              "1948" = "1948",
+              "1958" = "1958",
+              "1968" = "1968",
+              "1978" = "1978",
+              "1988" = "1988",
+              "1998" = "1998",
+              "2008" = "2008",
+              "2018" = "2018"
+              )
+INCOME_UNIT <- 10000000
+regions <- list("Asia" = "Asia",
+                "Europe" = "Europe",
+                "Africa" = "Africa",
+                "Americas" = "Americas",
+                "Oceania" = "Oceania"
+                )
+    
 COUNTRIES <- unique(df$country)
 
 app$layout(
     dbcContainer(
         list(
             dccGraph(id='plot-area'),
-            dccDropdown(
-                id='region_dropdown',
-                options = REGIONS, 
-                value='')
+            htmlLabel("Zoom in Years: "),
+            dccRangeSlider(
+                id = 'year_slider',
+                min = 1901, 
+                max = 2018,
+                value = list(1901, 2018),
+                marks = years
+            )
+            # ,
+            # htmlLabel("Filter by Geographic Region: "),
+            # dccDropdown(
+            #     id = 'region_dropdown',
+            #     options = regions , 
+            #     value = '')
         )
     )
 )
 
 app$callback(
     output('plot-area', 'figure'),
-    list(input('region_dropdown', 'value')),
-    function(region) {
-        p <- df %>% 
-            group_by(year) %>% 
+    list(input('year_slider', 'value')
+         # ,
+         # input('region_dropdown', 'value')
+         ),
+    
+    function(year_range) {
+    
+        p <- df %>%
+            filter(year >= year_range[1] & year <= year_range[2]) %>%
+            group_by(year) %>%
             summarise(income = sum(income),
-                      pop = sum(population)) %>% 
-            mutate(income_per_capita = income * INCOME_UNIT / pop) %>% 
+                      pop = sum(population)) %>%
+            mutate(income_per_capita = income * INCOME_UNIT / pop) %>%
             ggplot(aes(x = year, y = income_per_capita)) +
-            geom_line() 
+            geom_line() +
+            labs(x = "Year",
+                 y = "Income Per Capita (in US$)",
+                 title = "Income Per Capita Has Been Rising") +
+            ggthemes::scale_color_tableau()
+
         ggplotly(p)
+        
+
     }
+    # function(time) {
+    #     df_filter_year <- df %>% 
+    #         filter(year >= years[time[1] + 1] & year <= years[time[2] + 1])
+    #     
+    #     if (region == '') {
+
+    #     } else {
+    #         p <- df_filter_year %>%
+    #             filter(region == region) %>%
+    #             group_by(year) %>%
+    #             summarise(income = sum(income),
+    #                       pop = sum(population)) %>%
+    #             mutate(income_per_capita = income * INCOME_UNIT / pop) %>%
+    #             ggplot(aes(x = year, y = income_per_capita)) +
+    #             geom_line()
+    #         ggplotly(p)
+    #     }
+    # }
 )
 
 app$run_server(host = '0.0.0.0')
